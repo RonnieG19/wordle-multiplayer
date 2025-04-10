415ca46c0818be8515dbdf842174df2efacdba52
RonnieG19 <65129548+RonnieG19@users.noreply.github.com>
2025-04-10 16:36:03 +0100

    Update socket.js

--- wordle-multiplayer/client/src/socket.js
+++ wordle-multiplayer/client/src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:4000"); // Change this to your Render backend URL for production
+const socket = io("https://wordle-multiplayer.onrender.com"); // Change this to your Render backend URL for production
 export default socket;
